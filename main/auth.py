04332b66c2e988d4bac3c6ae7ca63476ad8d4120
Ian <iangu234@gmail.com>
2020-03-29 22:21:30 -0400

    move authentication to blueprint

---
+++ main/auth.py
@@ -0,0 +1,66 @@
+from flask import Blueprint, render_template
+import flask
+import spotify.sync as spotify
+from models import ListeningSession
+import os
+import string
+import random
+from typing import Dict
+
+
+bp = Blueprint('auth', __name__, template_folder='templates')
+
+#  Client Keys
+SPOTIFY_CLIENT_ID = os.environ['SPOTIFY_CLIENT_ID']
+SPOTIFY_CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
+REDIRECT_URI = os.environ['REDIRECT_URI']
+
+SPOTIFY_CLIENT = spotify.Client(SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET)
+
+OAUTH2_SCOPES = ('user-modify-playback-state',
+                 'user-read-currently-playing',
+                 'user-read-playback-state',
+                 'user-read-playback-state',
+                 'user-modify-playback-state',
+                 'user-read-currently-playing',
+                 'streaming app-remote-control',
+                 'playlist-read-collaborative',
+                 'playlist-modify-private',
+                 'playlist-read-private',
+                 'playlist-modify-public',
+                 'user-library-read',
+                 'playlist-read-private',
+                 'user-top-read',
+                 'user-read-recently-played',
+                 'user-follow-read',
+                 'user-follow-modify')
+OAUTH2: spotify.OAuth2 = spotify.OAuth2(SPOTIFY_CLIENT.id, REDIRECT_URI, scopes=OAUTH2_SCOPES)
+
+SPOTIFY_USERS: Dict[str, spotify.User] = {}
+listeningSessions: Dict[str, ListeningSession] = {}
+partyIdMap: Dict[str, str] = {}
+
+
+@bp.route('/spotify/callback')
+def spotify_callback():
+    try:
+        code = flask.request.args['code']
+    except KeyError:
+        return flask.redirect('/spotify/failed')
+    else:
+        key = ''.join(random.choice(string.ascii_uppercase) for _ in range(16))
+        while key in SPOTIFY_USERS:
+            key = ''.join(random.choice(string.ascii_uppercase) for _ in range(16))
+
+        # noinspection PyTypeChecker
+        SPOTIFY_USERS[key] = spotify.User.from_code(SPOTIFY_CLIENT, code, redirect_uri=REDIRECT_URI)
+
+        flask.session['spotify_user_id'] = key
+
+    return flask.redirect('/')
+
+
+@bp.route('/spotify/failed')
+def spotify_failed():
+    flask.session.pop('spotify_user_id', None)
+    return 'Failed to authenticate with Spotify.'
--- main/main.py
+++ main/main.py
@@ -1,70 +1,17 @@
-from models import ListeningSession
 import os
-import string
 import random
-from typing import Dict
+import string
+
 import flask
 import spotify.sync as spotify
-
-
-#  Client Keys
-SPOTIFY_CLIENT_ID = os.environ['SPOTIFY_CLIENT_ID']
-SPOTIFY_CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
-
-SPOTIFY_CLIENT = spotify.Client(SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET)
+import auth
+from auth import SPOTIFY_CLIENT, SPOTIFY_USERS, listeningSessions, OAUTH2, partyIdMap
+from models import ListeningSession
 
 APP = flask.Flask(__name__)
+APP.register_blueprint(auth.bp)
 APP.secret_key = os.environ['SESSION_SECRET_KEY']
 APP.config.from_mapping({'spotify_client': SPOTIFY_CLIENT})
-
-REDIRECT_URI = os.environ['REDIRECT_URI']
-OAUTH2_SCOPES = ('user-modify-playback-state',
-                 'user-read-currently-playing',
-                 'user-read-playback-state',
-                 'user-read-playback-state',
-                 'user-modify-playback-state',
-                 'user-read-currently-playing',
-                 'streaming app-remote-control',
-                 'playlist-read-collaborative',
-                 'playlist-modify-private',
-                 'playlist-read-private',
-                 'playlist-modify-public',
-                 'user-library-read',
-                 'playlist-read-private',
-                 'user-top-read',
-                 'user-read-recently-played',
-                 'user-follow-read',
-                 'user-follow-modify')
-OAUTH2: spotify.OAuth2 = spotify.OAuth2(SPOTIFY_CLIENT.id, REDIRECT_URI, scopes=OAUTH2_SCOPES)
-
-SPOTIFY_USERS: Dict[str, spotify.User] = {}
-listeningSessions: Dict[str, ListeningSession] = {}
-partyIdMap: Dict[str, str] = {}
-
-
-@APP.route('/spotify/callback')
-def spotify_callback():
-    try:
-        code = flask.request.args['code']
-    except KeyError:
-        return flask.redirect('/spotify/failed')
-    else:
-        key = ''.join(random.choice(string.ascii_uppercase) for _ in range(16))
-        while key in SPOTIFY_USERS:
-            key = ''.join(random.choice(string.ascii_uppercase) for _ in range(16))
-
-        # noinspection PyTypeChecker
-        SPOTIFY_USERS[key] = spotify.User.from_code(SPOTIFY_CLIENT, code, redirect_uri=REDIRECT_URI)
-
-        flask.session['spotify_user_id'] = key
-
-    return flask.redirect('/')
-
-
-@APP.route('/spotify/failed')
-def spotify_failed():
-    flask.session.pop('spotify_user_id', None)
-    return 'Failed to authenticate with Spotify.'
 
 
 @APP.route('/')
